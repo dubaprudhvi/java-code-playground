fdbf3db01b4250b4cdab9e9e8b177fa151c6a83a
dubaprudhvi <dubaprudhvi2000@gmail.com>
2025-12-06 22:33:40 +0530

    Added Insertion sort

--- src/main/java/dsa/java/code/playground/sort/InsertionSort.java
+++ src/main/java/dsa/java/code/playground/sort/InsertionSort.java
@@ -32,12 +32,12 @@ class InsertionSort {
     }
 
     private int[] insertionSort(int[] arr) {
-        for(int i=0; i<arr.length; i++){
-            int j =i;
-            while(j>0 && arr[j-1]>arr[j]){
+        for (int i = 0; i < arr.length; i++) {
+            int j = i;
+            while (j > 0 && arr[j - 1] > arr[j]) {
                 int temp = arr[j];
-                arr[j] = arr[j-1];
-                arr[j-1] = temp;
+                arr[j] = arr[j - 1];
+                arr[j - 1] = temp;
                 j--;
             }
         }
